07843bd6812cb80820a61543d48c7471759b1d0d
BreezeEzy <snoopstrapper@gmail.com>
2026-02-24 17:07:20 +0400

    Warning Interface created

---
+++ Source/HorovodShooter/Private/Interfaces/WarningRecieverInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/WarningRecieverInterface.h"
+
+// Add default functionality here for any IWarningRecieverInterface functions that are not pure virtual.
